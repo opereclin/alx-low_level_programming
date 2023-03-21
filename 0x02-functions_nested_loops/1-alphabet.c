17486c50d6c8699cb2e7581c4c7dcb0e2e9cdc25
opereclin <opereclinton583@gmail.com>
2023-03-21 16:04:12 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_alphabet - prints out the alphabet in lowercase
+*
+*/
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = 97 ; x <= 122 ; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
