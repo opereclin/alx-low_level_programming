aa83fe3a9b916416d3c8d11b4a92164e9f9c4100
opereclin <opereclinton583@gmail.com>
2023-03-21 16:14:27 -0700

    task 1

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - check if @i is a alpha character
+ *
+ * @i: the character to check
+ *
+ * Return: 1 if @i is an alpha character
+ * else return 0
+ */
+int _isalpha(int i)
+{
+	return ((i >= 97 && i <= 122) || (i >= 65 && i <= 90) ? 1 : 0);
+}
