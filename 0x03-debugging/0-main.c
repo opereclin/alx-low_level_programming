9444dc239fb8813286f57bbf1f8f1d1c7131df0c
opereclin <opereclinton583@gmail.com>
2023-03-24 05:19:00 -0700

    debug

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main - tests the positive_negative function
+*
+*Return: return 0
+*/
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
