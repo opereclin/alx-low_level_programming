c41f021a1c6e6843a16f2409b70bf1e6fdd1fd13
opereclin <opereclinton583@gmail.com>
2023-03-24 05:47:11 -0700

    more functions

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void print_diagonal(int n);
+int _isupper(int c);
+int _isdigit(int i);
+int mul(int a, int b);
+void print_numbers(void);
+int _putchar(char);
+void print_most_numbers(void);
+void print_line(int n);
+void print_square(int size);
+void more_numbers(void);
+#endif /* MAIN_H */
