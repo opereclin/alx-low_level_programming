fe5cdb10a1d44772bce749dfa0177b8604892539
opereclin <opereclinton583@gmail.com>
2023-03-25 10:48:33 -0700

    nested loops

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -13,4 +13,5 @@ void print_most_numbers(void);
 void print_line(int n);
 void print_square(int size);
 void more_numbers(void);
+void print_triangle(int size);
 #endif /* MAIN_H *int mul(int a, int b);/
