e1335abf27de1e46c6ccb158d63bffe0f39a9295
opereclin <opereclinton583@gmail.com>
2023-03-25 11:21:15 -0700

    nested

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,19 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-
-void print_diagonal(int n);
+int _putchar(char c);
 int _isupper(int c);
-int _isdigit(int i);
+int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
-int _putchar(char);
 void print_most_numbers(void);
-void print_line(int n);
-void print_square(int size);
 void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 
-#endif /* MAIN_H *int mul(int a, int b);/
+#endif
