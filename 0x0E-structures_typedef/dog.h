dc9090fcfbc8eb66e6eea48c3c4ca4822022bc95
opereclin <opereclinton583@gmail.com>
2023-04-17 16:23:36 -0700

    structures_typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,28 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Type describing dog.
+ *
+ * @name: String, name of dog.
+ * @age: Float, age of dog.
+ * @owner: String, dog owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - typedef of struct dog.
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
